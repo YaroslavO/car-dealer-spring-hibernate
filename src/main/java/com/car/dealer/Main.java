8c77795fac504035c83ed972ad7e0a623d53dc6c
motomen <gdpdp.ru@mail.ru>
2015-05-31 04:09:15 +0300

    init commit

---
+++ src/main/java/com/car/dealer/Main.java
@@ -0,0 +1,15 @@
+package com.car.dealer;
+
+import com.car.dealer.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext appContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        CarDealer carDealer = (CarDealer) appContext.getBean("carDealer");
+        carDealer.init();
+    }
+}
